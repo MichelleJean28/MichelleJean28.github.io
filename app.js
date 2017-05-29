32b314c76fb867b4a7fcc8f0456251d6f700ea74
Michelle <contandrei@yahoo.com>
2017-05-29 15:48:57 +0300

    h
    h

--- app.js
+++ app.js
@@ -55,7 +55,7 @@
                 var statesLayer = new TileLayer({url: "https://tiles.arcgis.com/tiles/6FZUQ16zBFwjeNsQ/arcgis/rest/services/bucuresti2_WTL2/MapServer"});
 
 
-                map.addLayer(statesLayer);
+                
             
             
             
