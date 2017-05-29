e3c403042123bdf88d79af348af8e6ede22d3480
Michelle <contandrei@yahoo.com>
2017-05-29 18:59:56 +0300

    w
    w

--- app.js
+++ app.js
@@ -55,7 +55,7 @@
             
             
             
-                var statesLayer = new ArcGISDynamicMapServiceLayer ("https://tiles.arcgis.com/tiles/6FZUQ16zBFwjeNsQ/arcgis/rest/services/bucuresti2_WTL2/MapServer/Layers", {
+                var statesLayer = new ArcGISDynamicMapServiceLayer ("https://tiles.arcgis.com/tiles/6FZUQ16zBFwjeNsQ/arcgis/rest/services/bucuresti2_WTL2/MapServer", {
                 });
 
                 map.addLayer(statesLayer);            
